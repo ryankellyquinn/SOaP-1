5b15afb2903606a6ccc82f6288deee1e898c3833
Zoey Werbin <zoeywerbin@gmail.com>
2019-03-18 23:48:53 -0400

    condensed visualization scripts for NEON soil data

--- data_construction/NEON_covariates/NEON_soil_visualization.R
+++ data_construction/NEON_covariates/NEON_soil_visualization.R
@@ -1,7 +1,14 @@
 library("ggplot2")
 library("scales")
-soil_chm_merge <- readRDS("NEON_soil_chm_merge.rds")
 
+source("data_construction/NEON_covariates/NEON_soil_phys_iterate.R")
+source("data_construction/NEON_covariates/NEON_soil_chem_iterate.R")
+soil_phys_merge <- readRDS("data/NEON_soil_phys_merge.rds")
+soil_chm_merge <- readRDS("data/NEON_soil_chm_merge.rds")
+
+# first, visualize soil chemistry data 
+
+# organic carbon
 for (i in 1:length(sites)){
   soil_chm_site <- soil_chm_merge[which(soil_chm_merge$siteID==sites[i]),]
   chm.time <-as.Date(soil_chm_site$collectDate, format = "%Y-%m-%d")
@@ -11,6 +18,7 @@ for (i in 1:length(sites)){
   print(p + ggtitle(paste0(sites[i], "   % Organic C")))
 }
 
+# percent nitrogen
 for (i in 1:length(sites)){
   soil_chm_site <- soil_chm_merge[which(soil_chm_merge$siteID==sites[i]),]
   chm.time <-as.Date(soil_chm_site$collectDate, format = "%Y-%m-%d")
@@ -20,6 +28,7 @@ for (i in 1:length(sites)){
   print(p + ggtitle(paste0(sites[i], "   % N")))
 }
 
+# C:N ratio
 for (i in 1:length(sites)){
   soil_chm_site <- soil_chm_merge[which(soil_chm_merge$siteID==sites[i]),]
   chm.time <-as.Date(soil_chm_site$collectDate, format = "%Y-%m-%d")
@@ -29,3 +38,45 @@ for (i in 1:length(sites)){
   print(p + ggtitle(paste0(sites[i], "   C:N Ratio")))
 }
 
+
+# now we'll look at physical measurements
+
+# soil moisture
+for (i in 1:length(sites)){
+  soil_phys_site <- soil_phys_merge[which(soil_phys_merge$siteID==sites[i]),]
+  chm.time <-as.Date(soil_phys_site$collectDate, format = "%Y-%m-%d")
+  p <- ggplot(soil_phys_site, aes(x = chm.time, y = soil_phys_site$soilMoisture)) + 
+    geom_point(aes(colour = soil_phys_site$plotID, fill = soil_phys_site$plotID)) + 
+    scale_x_date("", labels = date_format("%b %Y"))
+  print(p + ggtitle(paste0(sites[i], "Soil Moisture")))
+}
+
+# soil temperature
+for (i in 1:length(sites)){
+  soil_phys_site <- soil_phys_merge[which(soil_phys_merge$siteID==sites[i]),]
+  chm.time <-as.Date(soil_phys_site$collectDate, format = "%Y-%m-%d")
+  p <- ggplot(soil_phys_site, aes(x = chm.time, y = soil_phys_site$soilTemp)) + 
+    geom_point(aes(colour = soil_phys_site$plotID, fill = soil_phys_site$plotID)) + 
+    scale_x_date("", labels = date_format("%b %Y"))
+  print(p + ggtitle(paste0(sites[i], "   Soil Temperature"))) 
+}
+
+# standing water depth
+for (i in 1:length(sites)){
+  soil_phys_site <- soil_phys_merge[which(soil_phys_merge$siteID==sites[i]),]
+  chm.time <-as.Date(soil_phys_site$collectDate, format = "%Y-%m-%d")
+  p <- ggplot(soil_phys_site, aes(x = chm.time, y = soil_phys_site$standingWaterDepth)) + 
+    geom_point(aes(colour = soil_phys_site$plotID, fill = soil_phys_site$plotID)) + 
+    scale_x_date("", labels = date_format("%b %Y"))
+  print(p + ggtitle(paste0(sites[i], "   Standing Water Depth ")))
+}
+
+# litter depth
+for (i in 1:length(sites)){
+  soil_phys_site <- soil_phys_merge[which(soil_phys_merge$siteID==sites[i]),]
+  chm.time <-as.Date(soil_phys_site$collectDate, format = "%Y-%m-%d")
+  p <- ggplot(soil_phys_site, aes(x = chm.time, y = soil_phys_site$litterDepth)) + 
+    geom_point(aes(colour = soil_phys_site$plotID, fill = soil_phys_site$plotID)) + 
+    scale_x_date("", labels = date_format("%b %Y"))
+  print(p + ggtitle(paste0(sites[i], "   litter depth")))
+}
